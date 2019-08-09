8a7a4f22ce0b27e6cefc1c0d6ec0e8eb5ba27730
Andrew <siricoa94@github.com>
2019-08-09 13:29:06 -0400

    added event listener

--- assets/js/server.js
+++ assets/js/server.js
@@ -8,3 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var connection = require("./connection.js");
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
