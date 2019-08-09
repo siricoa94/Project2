d6ed2b831478b0fb647b96dfb3f6295320fa3a3c
Eric Thiel <ethiel172@gmail.com>
2019-08-09 11:06:17 -0400

    added comment reminder to update event listener

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,6 +4,9 @@ var API = "DHI2B31EUTFJ9PV7";
 var symbol = "MSFT";
 var interval = "60min";
 var JSONresponse = $("#jsonresponse");
+// event listener for any button click.
+//  NEEDS TO BE CHANGED WHEN FRONT END LAYOUT FINALIZED
+//  ONLY WORKS WITH open-page.html FILE
 
 $(document).on('click', 'button', function () {
   var API = "DHI2B31EUTFJ9PV7";
