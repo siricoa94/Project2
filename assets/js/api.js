c22af72414bfbc83db8606bad476f53059644a5f
Eric Thiel <ethiel172@gmail.com>
2019-08-09 11:04:00 -0400

    API call for stock info framework

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,10 +3,17 @@
 var API = "DHI2B31EUTFJ9PV7";
 var symbol = "MSFT";
 var interval = "60min";
+var JSONresponse = $("#jsonresponse");
 
-$.ajax({
-  url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ symbol + "&interval="+ interval + "&apikey=" + API,
-  method: "GET"
-}).then(function (response) {
-  console.log(response);
+$(document).on('click', 'button', function () {
+  var API = "DHI2B31EUTFJ9PV7";
+  var symbol = "MSFT";
+  var interval = "60min";
+
+  $.ajax({
+    url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=" + interval + "&apikey=" + API,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
 });
