4399d8b472836a423b1b72e25d65e10bf92f6588
Eric Thiel <ethiel172@gmail.com>
2019-08-09 10:47:18 -0400

    swapped index and open-page

---
+++ assets/js/api.js
@@ -0,0 +1,12 @@
+// Alpha Vantage API call
+
+var API = "DHI2B31EUTFJ9PV7";
+var symbol = "MSFT";
+var interval = "60min";
+
+$.ajax({
+  url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+ symbol + "&interval="+ interval + "&apikey=" + API,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+});
