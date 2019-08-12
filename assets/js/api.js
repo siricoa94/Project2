e0f61c1dc329db4af796f709ff248b467ee5f9f9
Eric Thiel <ethiel172@gmail.com>
2019-08-12 19:06:40 -0400

    added NYT articles that load when page loads

--- assets/js/api.js
+++ assets/js/api.js
@@ -32,21 +32,9 @@ $(document).on('click', '#stockSearchButton', function () {
 
 
 // New York Times API call and response manipulation
-// Build Query URL
-function buildQueryURL() {
-  //The possible section value are: arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, and world.
-
-
-  console.log(queryURL);
-  return queryURL;
-
-
-};
-
 function updatePage(NYTData) {
-  // Get from the form the number of results to display
-  // API doesn't have a "limit" parameter, so we have to do this ourselves
-  var numArticles = 10;
+  // Set Limit of articles to display
+  var numArticles = 3;
 
   // Log the NYTData to console, where it will show up as an object
   console.log(NYTData);
@@ -55,10 +43,7 @@ function updatePage(NYTData) {
   // Loop through and build elements for the defined number of articles
   for (var i = 0; i < numArticles; i++) {
     // Get specific article info for current index
-    var article = NYTData.response.docs[i];
-
-    // Increase the articleCount (track article # - starting at 1)
-    var articleCount = i + 1;
+    var article = NYTData.results[i];
 
     // Create the  list group to contain the articles and add the article content for each
     var $articleList = $("<ul>");
@@ -68,46 +53,36 @@ function updatePage(NYTData) {
     $("#article-section").append($articleList);
 
     // If the article has a headline, log and append to $articleList
-    var headline = article.headline;
-    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
-
-    if (headline && headline.main) {
-      console.log(headline.main);
+    var headline = article.title;
+    var $articleListItem = $("<li class='list-group-item articleHeadline m-1'>");
+    if (headline) {
       $articleListItem.append(
-        "<span class='label label-primary'>" +
-        articleCount +
-        "</span>" +
         "<strong> " +
-        headline.main +
+        headline +
         "</strong>"
       );
     }
-
     // If the article has a byline, log and append to $articleList
     var byline = article.byline;
 
-    if (byline && byline.original) {
-      console.log(byline.original);
-      $articleListItem.append("<h5>" + byline.original + "</h5>");
+    if (byline) {
+      $articleListItem.append("<h5>" + byline + "</h5>");
     }
 
     // Log section, and append to document if exists
-    var section = article.section_name;
-    console.log(article.section_name);
+    var section = article.section;
     if (section) {
       $articleListItem.append("<h5>Section: " + section + "</h5>");
     }
 
     // Log published date, and append to document if exists
-    var pubDate = article.pub_date;
-    console.log(article.pub_date);
+    var pubDate = article.published_date;
     if (pubDate) {
-      $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+      $articleListItem.append("<h5>" + pubDate + "</h5>");
     }
 
     // Append and log url
-    $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
-    console.log(article.web_url);
+    $articleListItem.append("<a href='" + article.url + "'>" + article.url + "</a>");
 
     // Append the article
     $articleList.append($articleListItem);
@@ -127,15 +102,22 @@ $(document).ready(function () {
   
   // Empty the region associated with the articles
   clear();
+
+
   // This is for dev purposes only. Should be removed when data has been formatted to page.
   // (so we don't keep making an API call every time we refresh)
   $.getJSON("https://api.myjson.com/bins/pic7v", function (data) {
-      console.log(data)
+    console.log("THIS RESPONSE IS FROM A MYJSON SITE. NOT A REAL AJAX CALL")
+    console.log("REMOVE BEFORE FINAL DEPLOYMENT")
     }).then(updatePage);
-    //THIS IS THE AJAX CALL THE FINAL PRODUCT WILL USE 
+
+
+
+  //THIS IS THE AJAX CALL THE FINAL PRODUCT WILL USE 
   // Make the AJAX request to the API - GETs the JSON data at the queryURL.
   // The data then gets passed as an argument to the updatePage function
   // var queryURL = "https://api.nytimes.com/svc/topstories/v2/business.json?api-key=DK2EnCjcfrg3Zv1nG8Ym1eCuJQJZa6xe";
+  //The possible section value are: arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, and world.
   // $.ajax({
   //   url: queryURL,
   //   method: "GET"
