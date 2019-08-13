a667c29a5f6bbb3ed295f2893b435d6141cb96fa
Andrew <siricoa94@github.com>
2019-08-13 17:00:01 -0400

    ignore

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,6 +1,9 @@
+var path = require("path");
+module.exports = function(app) {
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/assets/html/index.html"));
 });
 app.get("/open-page", function (req, res) {
-    res.sendFile(path.join(__dirname, "/open-page.html"));
+    res.sendFile(path.join(__dirname, "/assets/html/open-page.html"));
 });
+}
