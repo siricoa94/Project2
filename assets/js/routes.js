5159bce11a24656807dfeb1c948e2a951649bbf3
Eric Thiel <48534582+bigPoppaChop@users.noreply.github.com>
2019-08-14 11:03:55 -0400

    Revert "Andy branch"

--- assets/js/routes.js
+++
@@ -1,9 +0,0 @@
-var path = require("path");
-module.exports = function(app) {
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/assets/html/index.html"));
-});
-app.get("/open-page", function (req, res) {
-    res.sendFile(path.join(__dirname, "/assets/html/open-page.html"));
-});
-}
