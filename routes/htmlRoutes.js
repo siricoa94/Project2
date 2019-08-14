bf38aa83f7b2b4ab6c47d5f7a2d8392a93bce227
Andrew <siricoa94@github.com>
2019-08-14 11:03:40 -0400

    got routes working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,11 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../assets/html/index.html"));
- });
-//  app.get("/open-page", function (req, res) {
-//     res.sendFile(path.join(__dirname, "../assets/html/open-page.html"));
-//  });
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
+    });
+     app.get("/open-page", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/assets/html/open-page.html"));
+     });
 };
-
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './public')));
 // Routes
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
